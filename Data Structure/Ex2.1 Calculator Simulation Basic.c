b9003886311c4b45957ebdbba0481696ea474027
Oh MeWho <ohmewho@users.noreply.github.com>
2017-08-11 22:38:16 +0800

    Create Ex2.1 Calculator Simulation Basic.c

---
+++ Data Structure/Ex2.1 Calculator Simulation Basic.c
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+
+int main(){
+	
+	int num, result, error = 0;
+	char ch; 
+	scanf("%d %c", &num, &ch);
+	result = num;
+	while(ch != '='){
+		scanf("%d", &num);
+		switch(ch){
+			case '+': result += num;
+			break;
+			case '-': result -= num;
+			break;
+			case '*': result *= num;
+			break;
+			case '/': if(num == 0) error = 1;
+					  else result /= num;
+					  break;
+			default: error = 1;
+			break;
+		}
+		scanf("%c", &ch);
+		
+	}
+	if(error) printf("ERROR\n");
+	else printf("%d\n", result);
+	
+	return 0;
+}
